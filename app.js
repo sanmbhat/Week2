c178846470a534f488e480d37e209365164910a3
santosh <santosh.bhat87@gmail.com>
2018-04-03 19:50:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = angular.module("ShoppingListCheckOff", [])
+.controller("ToBuyController", ToBuyController)
+.controller("AlreadyBoughtController", AlreadyBoughtController)
+.service("ShoppingListCheckOffService", function() {
+	this.toBuyItems = [{name: "Apples", quantity:"10"}, {name: "Bananas", quantity:"2"}, {name: "Mangos", quantity:"5"}, {name: "Strawberries", quantity:"3"}, {name: "Grapes", quantity:"100"}];
+	this.boughtItems = [];
+	this.buy = function(index) {
+		var item = this.toBuyItems[index];
+		this.toBuyItems = this.toBuyItems.splice(index, 1);
+		this.boughtItems.push(item);
+	}
+});
+
+ToBuyController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+function ToBuyController($scope, ShoppingListCheckOffService) {
+	$scope.buyList = ShoppingListCheckOffService.toBuyItems;
+	$scope.BuyItem = function(index) {
+		ShoppingListCheckOffService.buy(index);
+	}
+}
+
+AlreadyBoughtController.$inject = ['$scope', 'ShoppingListCheckOffService'];
+function AlreadyBoughtController($scope, ShoppingListCheckOffService) {
+	$scope.boughtList = ShoppingListCheckOffService.boughtItems;
+}
+
